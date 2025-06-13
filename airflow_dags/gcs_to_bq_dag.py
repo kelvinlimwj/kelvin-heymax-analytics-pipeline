2976afd400055f1f681fc8c6c8c90b9ff78adcfa
Kelvin Lim <kelvin.limwj@gmail.com>
2025-06-13 12:11:57 +0000

    updated region

--- airflow_dags/gcs_to_bq_dag.py
+++ airflow_dags/gcs_to_bq_dag.py
@@ -62,9 +62,10 @@ with DAG(
             "source": {
                 "repo_source": {  
                     "project_id": "heymax-kelvin-analytics",
-                    "repo_name": "kelvin-heymax-analytics-pipeline",
+                    "repo_name": "kelvinlimwj-kelvin-heymax-analytics-pipeline",
                     "branch_name": "main",
-                    "dir": "dbt/dbt_bigquery_analytics"
+                    "dir": "dbt/dbt_bigquery_analytics",
+                    "region": "asia-southeast1"
                 }
             },
             "steps": [
