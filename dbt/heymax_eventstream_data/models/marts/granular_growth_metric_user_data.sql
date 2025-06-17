afba7ce99585af44e3bd225583358941baf10673
kelvinlimwj <55191847+kelvinlimwj@users.noreply.github.com>
2025-06-17 15:26:57 +0800

    Update granular_growth_metric_user_data.sql

--- dbt/heymax_eventstream_data/models/marts/granular_growth_metric_user_data.sql
+++ dbt/heymax_eventstream_data/models/marts/granular_growth_metric_user_data.sql
@@ -8,6 +8,6 @@ SELECT
     WHEN e.miles_amount IS NULL THEN 'engagement'
     ELSE 'miles_activity'
   END AS event_category
-FROM `heymax-kelvin-analytics.heymax_analytics.fct_events` AS e
-LEFT JOIN `heymax-kelvin-analytics.heymax_analytics.dim_users` AS u
+FROM {{ ref('fct_events') }} AS e
+LEFT JOIN {{ ref('dim_users') }} AS u
   ON e.user_id = u.user_id
