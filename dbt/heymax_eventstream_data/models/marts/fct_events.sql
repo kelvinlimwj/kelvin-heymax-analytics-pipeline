b88bc7535716852cf59c0939379cf79d36870ced
kelvinlimwj <55191847+kelvinlimwj@users.noreply.github.com>
2025-06-17 15:52:50 +0800

    Update fct_events.sql

--- dbt/heymax_eventstream_data/models/marts/fct_events.sql
+++ dbt/heymax_eventstream_data/models/marts/fct_events.sql
@@ -1,9 +1,16 @@
 SELECT
   user_id,
   event_time,
+  DATE(e.event_time) AS event_day,
+  FORMAT_DATE('%G-W%V', DATE(e.event_time)) AS event_week,
+  FORMAT_DATE('%Y-%m', DATE(e.event_time)) AS event_month,
   event_type,
   platform,
   utm_source,
   transaction_category,
-  miles_amount
+  miles_amount,
+  CASE 
+    WHEN e.miles_amount IS NULL THEN 'engagement'
+    ELSE 'miles_activity'
+  END AS event_category
 FROM {{ ref('stg_event_stream') }}
