84e88f449b1e4b486720789a983690081ad894a7
Kelvin Lim <kelvin.limwj@gmail.com>
2025-06-18 10:14:45 +0000

    Updated SQL Logic

--- dbt/heymax_dbt_data_tables/models/marts/analytics_tables/user_attrition_data.sql
+++ dbt/heymax_dbt_data_tables/models/marts/analytics_tables/user_attrition_data.sql
@@ -3,25 +3,27 @@
 ) }}
 
 WITH user_data AS (
-    SELECT 
-  e.*,
-  u.country
-FROM {{ ref('fct_events') }} AS e
-LEFT JOIN {{ ref('dim_users') }} AS u
-  ON e.user_id = u.user_id
+  SELECT 
+    e.*,
+    u.country
+  FROM {{ ref('fct_events') }} AS e
+  LEFT JOIN {{ ref('dim_users') }} AS u
+    ON e.user_id = u.user_id
 ),
 
 user_activity_by_week AS (
   SELECT
     user_id,
+    country,
     event_week
   FROM user_data
-  GROUP BY user_id, event_week
+  GROUP BY user_id, country, event_week
 ),
 
 activity_with_lag AS (
   SELECT
     user_id,
+    country,
     event_week,
     LAG(event_week) OVER (PARTITION BY user_id ORDER BY event_week) AS prev_week,
     LEAD(event_week) OVER (PARTITION BY user_id ORDER BY event_week) AS next_week
@@ -31,17 +33,17 @@ activity_with_lag AS (
 labeled_users AS (
   SELECT
     user_id,
+    country,
     event_week,
     prev_week,
     next_week,
     CASE
+      WHEN next_week IS NULL THEN 'Churned'
       WHEN prev_week IS NULL THEN 'New'
-      WHEN prev_week IS NOT NULL AND next_week IS NOT NULL THEN 'Retained'
-      WHEN prev_week IS NULL AND next_week IS NOT NULL THEN 'New'
-      WHEN prev_week IS NOT NULL AND next_week IS NULL THEN 'Churned'
-      WHEN prev_week IS NOT NULL AND event_week NOT IN (SELECT event_week FROM user_activity_by_week WHERE user_id = ua.user_id) THEN 'Resurrected'
+      WHEN DATE_DIFF(PARSE_DATE('%G-W%V', event_week), PARSE_DATE('%G-W%V', prev_week), WEEK) = 1 THEN 'Retained'
+      WHEN DATE_DIFF(PARSE_DATE('%G-W%V', event_week), PARSE_DATE('%G-W%V', prev_week), WEEK) > 1 THEN 'Resurrected'
     END AS user_status
-  FROM activity_with_lag ua
+  FROM activity_with_lag
 )
 
 SELECT * FROM labeled_users
