04efd19a6745ad7595864613ab6b9d1b2b2e2630
Kelvin Lim <kelvin.limwj@gmail.com>
2025-06-19 10:04:51 +0000

    Force worker identity

--- airflow_dags/log_gcp_identity.py
+++ airflow_dags/log_gcp_identity.py
@@ -1,26 +1,20 @@
-from airflow import DAG
 from airflow.operators.python import PythonOperator
-from datetime import datetime
-from google.auth import default
+from google.auth.transport.requests import Request
+from google.auth.compute_engine.credentials import Credentials
 
 def log_gcp_identity():
-    credentials, _ = default()
+    credentials = Credentials()
+    credentials.refresh(Request())
     print("🧠 Running as:", credentials.service_account_email)
 
-default_args = {
-    'owner': 'airflow',
-    'retries': 0,
-}
-
 with DAG(
-    dag_id='test_gcp_identity',
-    default_args=default_args,
+    dag_id='test_gcp_identity_force_metadata',
+    default_args={'owner': 'airflow'},
     start_date=datetime(2024, 1, 1),
     schedule_interval=None,
-    catchup=False,
-    tags=['test', 'gcp', 'identity'],
+    catchup=False
 ) as dag:
-
+    
     check_identity = PythonOperator(
         task_id='check_gcp_identity',
         python_callable=log_gcp_identity
